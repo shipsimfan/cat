9a5dea99df1812f4b55aa6cf2cf36be2d69b441b
Lance Hart <shipsimfan@hotmail.com>
2021-07-13 21:29:37 -0400

    Added reading of test file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,27 @@
+#include <los.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("CAT");
+    printf("CAT\n");
 
-    for (int i = 0; i < 16; i++)
-        printf("Cat - %i\n", i);
+    uint64_t fd = open_file("1:/TEST.TXT");
+    if (fd == 0xFFFFFFFFFFFFFFFF) {
+        printf("Error while opening file!\n");
+        return -1;
+    }
+
+    char* buffer = (char*)malloc(sizeof(char) * 32);
+    for (int i = 0; i < 32; i++)
+        buffer[i] = 0;
+
+    uint64_t bytes_read = read_file(fd, (void*)buffer, 32);
+    if (bytes_read == 0xFFFFFFFFFFFFFFFF) {
+        printf("Error while reading file!\n");
+        return -1;
+    }
+
+    printf("Message: %s\n", buffer);
 
     return 0xBADC0DE;
 }
