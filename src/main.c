b06a1b1b0c1edfe2bb52431921a6e4ff419d91cc
Lance Hart <shipsimfan@hotmail.com>
2021-07-17 23:12:56 -0400

    Added arguments

--- src/main.c
+++ src/main.c
@@ -2,26 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("CAT\n");
+#define BUFFER_SIZE 1025
 
-    uint64_t fd = open_file("1:/TEST.TXT");
+int main(int argc, const char** argv) {
+    if (argc != 2) {
+        printf("Usage: %s path\n", argv[0]);
+        exit(1);
+    }
+
+    uint64_t fd = open_file(argv[1]);
     if (fd == 0xFFFFFFFFFFFFFFFF) {
         printf("Error while opening file!\n");
         return -1;
     }
 
-    char* buffer = (char*)malloc(sizeof(char) * 32);
-    for (int i = 0; i < 32; i++)
-        buffer[i] = 0;
+    char* buffer = (char*)malloc(BUFFER_SIZE);
+    buffer[BUFFER_SIZE - 1] = 0;
 
-    uint64_t bytes_read = read_file(fd, (void*)buffer, 32);
-    if (bytes_read == 0xFFFFFFFFFFFFFFFF) {
-        printf("Error while reading file!\n");
-        return -1;
+    while (1) {
+        uint64_t bytes_read = read_file(fd, (void*)buffer, BUFFER_SIZE - 1);
+        if (bytes_read == 0xFFFFFFFFFFFFFFFF) {
+            printf("Error while reading file!\n");
+            return -1;
+        }
+
+        if (bytes_read == 0) {
+            break;
+        }
+
+        printf("%s", buffer);
     }
 
-    printf("Message: %s\n", buffer);
+    printf("\n");
 
-    return 0xBADC0DE;
+    return 0;
 }
