f21ff81d0225fa24dfb162e7f99657714df06e46
Lance Hart <shipsimfan@hotmail.com>
2021-07-25 16:31:27 -0400

    Added printing of clean error messages

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <los.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_SIZE 1025
 
@@ -12,7 +13,7 @@ int main(int argc, const char** argv) {
 
     int64_t fd = open_file(argv[1]);
     if (fd < 0) {
-        printf("Error while opening file: %li\n", fd);
+        printf("Error while opening file: %s\n", strerror(fd));
         exit(1);
     }
 
@@ -22,7 +23,7 @@ int main(int argc, const char** argv) {
     while (1) {
         int64_t bytes_read = read_file(fd, (void*)buffer, BUFFER_SIZE - 1);
         if (bytes_read < 0) {
-            printf("Error while reading file: %li\n", bytes_read);
+            printf("Error while reading file: %s\n", strerror(bytes_read));
             exit(1);
         }
 
